fb8711c1df3dd0960bf61ad49aef01777106e7a2
Shane Alton <shanealton1@gmail.com>
2017-06-05 18:59:51 -0400

    Fix babel presets and uglifyjs

--- react/react-basic/webpack.config.js
+++ react/react-basic/webpack.config.js
@@ -1,25 +1,25 @@
-var debug = process.env.NODE_ENV !== 'production';
 var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
   entry: __dirname + '/src/js/index.js',
+  output: {
+    path: __dirname + "/src/",
+    filename: "bundle.js"
+  },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
+        query: {
+                presets: ['react', 'es2015']
+            }
       }
     ]
   },
-  output: {
-    path: __dirname + "/src/",
-    filename: "bundle.js"
-  },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurrenceOrderPlugin(),
+  plugins: [
     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
   ],
 };
