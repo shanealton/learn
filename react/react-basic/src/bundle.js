08d09dd1bdcab057ab55ed4ae016c1e31e788cf2
Shane Alton <shanealton1@gmail.com>
2017-06-05 19:15:23 -0400

    Fix js style consistency

--- react/react-basic/src/bundle.js
+++ react/react-basic/src/bundle.js
@@ -9612,12 +9612,12 @@ var Header = function (_React$Component) {
   }
 
   _createClass(Header, [{
-    key: "render",
+    key: 'render',
     value: function render() {
       return _react2.default.createElement(
-        "h1",
+        'h1',
         null,
-        "Hello World!"
+        'Hello World!'
       );
     }
   }]);
@@ -9640,13 +9640,15 @@ var _react2 = _interopRequireDefault(_react);
 
 var _reactDom = __webpack_require__(82);
 
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
 var _App = __webpack_require__(81);
 
 var _App2 = _interopRequireDefault(_App);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-(0, _reactDom.render)(_react2.default.createElement(_App2.default, null), document.getElementById('app'));
+_reactDom2.default.render(_react2.default.createElement(_App2.default, null), document.getElementById('app'));
 
 /***/ }),
 /* 85 */
