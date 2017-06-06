4c68bdb60f44aa33f8a4cf680e01af825dbfdbb3
Shane Alton <shanealton1@gmail.com>
2017-06-06 18:12:03 -0400

    Refactor resource routes
    Refactor the api resource routes to be more modular and accept multiple controllers.

--- node/yak-yik/routes/api.js
+++ node/yak-yik/routes/api.js
@@ -1,70 +1,91 @@
 var express = require('express')
 var router = express.Router()
 var ZoneController = require('../controllers/ZoneController')
+var controllers = require('../controllers')
 
 router.get('/:resource', function(req, res, next) {
   var resource = req.params.resource
+  var controller = controllers[resource]
 
-  if (resource == 'zone') {
-    ZoneController.find(req.query, function(err, results) {
-      if (err) {
-        res.json({
-          confirmation: 'fail',
-          message: err
-        })
-        return
-      }
-
-      res.json({
-        confirmation: 'success',
-        results: results
-      })
+  if (controller == null) {
+    res.json({
+      confirmation: 'fail',
+      message: 'Invalid Resource Request: ' + resource
     })
+    return
   }
+
+  controller.find(req.query, function(err, results) {
+    if (err) {
+      res.json({
+        confirmation: 'fail',
+        message: err
+      })
+      return
+    }
+
+    res.json({
+      confirmation: 'success',
+      results: results
+    })
+  })
 })
 
 router.get('/:resource/:id', function(req, res, next) {
   var resource = req.params.resource,
-      id = req.params.id
+      id = req.params.id,
+      controller = controllers[resource]
 
-  if (resource == 'zone') {
-    ZoneController.findById(id, function(err, result) {
-      if (err) {
-        res.json({
-          confirmation: 'fail',
-          message: 'Not Found'
-        })
-        return
-      }
-
-      res.json({
-        confirmation: 'success',
-        result: result
-      })
+  if (controller == null) {
+    res.json({
+      confirmation: 'fail',
+      message: 'Invalid Resource Request: ' + resource
     })
+    return
   }
+
+  controller.findById(id, function(err, result) {
+    if (err) {
+      res.json({
+        confirmation: 'fail',
+        message: 'Not Found'
+      })
+      return
+    }
+
+    res.json({
+      confirmation: 'success',
+      result: result
+    })
+  })
 })
 
 router.post('/:resource', function(req, res, next) {
-  var resource = req.params.resource
+  var resource = req.params.resource,
+      controller = controllers[resource]
 
-  if (resource == 'zone') {
-    ZoneController.create(req.body, function(err, result) {
-      if (err) {
-        res.json({
-          confirmation: 'fail',
-          message: err
-        })
-        return
-      }
-
-      res.json({
-        confirmation: 'success',
-        result: result
-      })
+  if (controller == null) {
+    res.json({
+      confirmation: 'fail',
+      message: 'Invalid Resource Request: ' + resource
     })
+    return
   }
 
+  controller.create(req.body, function(err, result) {
+    if (err) {
+      res.json({
+        confirmation: 'fail',
+        message: err
+      })
+      return
+    }
+
+    res.json({
+      confirmation: 'success',
+      result: result
+    })
+  })
 })
 
 module.exports = router
