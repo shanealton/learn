b227dae9114f6e7ca70cf3aa36c7e66af08a7725
Shane Alton <shanealton1@gmail.com>
2017-06-07 00:51:16 -0400

    Setup react frontend

---
+++ node/yak-yik/src/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Zones from './components/Zones'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Zones />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ node/yak-yik/src/components/Zones.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Zones extends Component {
+  render() {
+    return (
+      <div>Zones</div>
+    )
+  }
+}
